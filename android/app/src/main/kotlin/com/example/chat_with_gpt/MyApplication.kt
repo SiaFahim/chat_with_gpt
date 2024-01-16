9bf3ac266d2b639d129bfd70488b9e8a019fab2d
siafahim <git@fahimi.me>
2024-01-16 16:38:37 -0600

    added android to firebase

---
+++ android/app/src/main/kotlin/com/example/chat_with_gpt/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.example.chat_with_gpt
+
+import androidx.multidex.MultiDexApplication
+
+class MyApplication : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize things here if needed
+    }
+}
